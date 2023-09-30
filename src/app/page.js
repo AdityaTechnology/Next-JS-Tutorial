bf9ee88299fc1dd5e07a342ddeb5f4170c0f940a
nextjs <nextjs@gmail.com>
2023-09-30 21:37:12 +0530

    tailwind css implementation

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,18 @@
 export default function Home() {
 	return (
 		<main>
-			<h1>Welcome to Next JS 13.5.3</h1>
+			<div>
+				<h1 className='text-4xl text-center text-blue-700 font-semibold'>
+					Introduction
+				</h1>
+			</div>
+			<div className='py-5 px-2 bg-orange-400 text-white rounded-xl  border border-red-700 mx-5 my-10'>
+				This is my tailwinds css tutorial
+			</div>
+			<div>
+				<p>this is my new paragraph. testing for tailwind css</p>
+			</div>
+			this is next line
 		</main>
 	);
 }
